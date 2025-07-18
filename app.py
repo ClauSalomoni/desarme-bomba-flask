2db7296ea4fbe2114f579b5906ff42e6a4e5df5d
Clau Salomoni <claudia404salomoni@gmail.com>
2025-07-18 13:00:42 -0300

    alterando porta

--- app.py
+++ app.py
@@ -11,4 +11,5 @@ app.secret_key = os.getenv("SECRET_KEY")
 from routes import *
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
